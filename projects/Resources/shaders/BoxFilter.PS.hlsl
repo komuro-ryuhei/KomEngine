3d2bcf39efa82e76690c4e44af2ab95c5c7525b0
komuro-ryuhei <k023g0040@g.neec.ac.jp>
2025-05-07 14:50:32 +0900

    smoothing実装

---
+++ projects/Resources/shaders/BoxFilter.PS.hlsl
@@ -0,0 +1,52 @@
+#include "Fullscreen.hlsli"
+
+Texture2D<float4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
+
+// -2〜+2までの5x5範囲を表すインデックス
+static const float2 kIndex5x5[5][5] =
+{
+    { { -2.0f, -2.0f }, { -1.0f, -2.0f }, { 0.0f, -2.0f }, { 1.0f, -2.0f }, { 2.0f, -2.0f } },
+    { { -2.0f, -1.0f }, { -1.0f, -1.0f }, { 0.0f, -1.0f }, { 1.0f, -1.0f }, { 2.0f, -1.0f } },
+    { { -2.0f, 0.0f }, { -1.0f, 0.0f }, { 0.0f, 0.0f }, { 1.0f, 0.0f }, { 2.0f, 0.0f } },
+    { { -2.0f, 1.0f }, { -1.0f, 1.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f }, { 2.0f, 1.0f } },
+    { { -2.0f, 2.0f }, { -1.0f, 2.0f }, { 0.0f, 2.0f }, { 1.0f, 2.0f }, { 2.0f, 2.0f } },
+};
+
+// すべて1/25で平均化
+static const float kKernel5x5[5][5] =
+{
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+    { 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f, 1.0f / 25.0f },
+};
+
+struct PixelShaderOutput
+{
+    float4 color : SV_TARGET0;
+};
+
+PixelShaderOutput main(VertexShaderOutput input)
+{
+    float width, height;
+    gTexture.GetDimensions(width, height);
+    float2 uvStepSize = float2(rcp(width), rcp(height));
+
+    PixelShaderOutput output;
+    output.color.rgb = float3(0.0f, 0.0f, 0.0f);
+    output.color.a = 1.0f;
+
+    for (uint x = 0; x < 5; ++x)
+    {
+        for (uint y = 0; y < 5; ++y)
+        {
+            float2 texcoord = input.texcoord + kIndex5x5[x][y] * uvStepSize;
+            float3 fetchColor = gTexture.Sample(gSampler, texcoord).rgb;
+            output.color.rgb += fetchColor * kKernel5x5[x][y];
+        }
+    }
+
+    return output;
+}
