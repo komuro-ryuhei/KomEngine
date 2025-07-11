3742f3a63d6ce7d042b21077f8fdd72b948842f0
komuro-ryuhei <k023g0040@g.neec.ac.jp>
2025-07-11 11:14:06 +0900

    一度コメントアウト

--- projects/Network.h
+++ projects/Network.h
@@ -4,19 +4,19 @@
 #include <string>
 #include <vector>
 
-using namespace web;
-using namespace web::http;
-using namespace web::http::client;
-
-class Network {
-public:
-	Network(const std::wstring& serverUrl);
-
-	pplx::task<void> Login(const std::wstring& username, const std::wstring& password);
-	pplx::task<int> PostScore(int score);
-	pplx::task<std::vector<int>> GetRanking();
-
-private:
-	std::wstring serverUrl;
-	std::wstring authToken;
-};
+//using namespace web;
+//using namespace web::http;
+//using namespace web::http::client;
+//
+//class Network {
+//public:
+//	Network(const std::wstring& serverUrl);
+//
+//	pplx::task<void> Login(const std::wstring& username, const std::wstring& password);
+//	pplx::task<int> PostScore(int score);
+//	pplx::task<std::vector<int>> GetRanking();
+//
+//private:
+//	std::wstring serverUrl;
+//	std::wstring authToken;
+//};
